793fbe3e4b5cb3bf61b32d820743457011df2f0f
micosara <micosara365@gamil.com>
2025-08-01 14:11:42 +0900

    8/1 spring_application : task complete

---
+++ spring_application/src/main/java/com/application/dao/RemoveFileSchedulerDAO.java
@@ -0,0 +1,10 @@
+package com.application.dao;
+
+import java.sql.SQLException;
+
+public interface RemoveFileSchedulerDAO {
+
+	int selectCountUsedByImageFromNotice(String fileName) throws SQLException;
+	int selectCountUsedByImageFromPDS(String fileName) throws SQLException;
+	int selectCountUsedByImageFromBoard(String fileName) throws SQLException;
+}
---
+++ spring_application/src/main/java/com/application/dao/RemoveFileSchedulerDAOImpl.java
@@ -0,0 +1,28 @@
+package com.application.dao;
+
+import java.sql.SQLException;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class RemoveFileSchedulerDAOImpl implements RemoveFileSchedulerDAO{
+
+	private SqlSession session;
+	public RemoveFileSchedulerDAOImpl(SqlSession session) {
+		this.session = session;
+	}
+	
+	@Override
+	public int selectCountUsedByImageFromNotice(String fileName) throws SQLException {
+		return session.selectOne("Notice-Mapper.selectCountUsedByImage",fileName);
+	}
+
+	@Override
+	public int selectCountUsedByImageFromPDS(String fileName) throws SQLException {
+		return session.selectOne("Pds-Mapper.selectCountUsedByImage",fileName);
+	}
+
+	@Override
+	public int selectCountUsedByImageFromBoard(String fileName) throws SQLException {
+		return session.selectOne("Board-Mapper.selectCountUsedByImage",fileName);
+	}
+}
